f46e7538037fe320b17c354a793526e78522254a
Shishumbaev <zhizhov1@gmail.com>
2021-04-08 13:37:22 +0300

    Start project day 2 rerun day 3 vse, krome my_var выбор результата Полностью переделал с 1 функцией гет working without global finish Начинаем работать по Доусону Программа отгадай число, отгадывает компьютер Счет интервал Обратная последовательность строки улучшенная анаграмма

--- Dawson.py
+++ Dawson.py
@@ -1,6 +1,29 @@
-text = str(input())
-print(len(text))
-new_text = ""
-for i in range(len(text)):
-    new_text += text[-i-1]
-print(new_text)
+#АНАГРАММА
+import random
+WORDS = ("питон", "анаграмма", "простая", "сложная", "ответ", "подстаканник")
+word = random.choice(WORDS)
+correct = word
+jumble = ""
+while word:
+    position = random.randrange(len(word))
+    jumble += word[position]
+    word = word[:position] + word[(position+1):]
+
+print("Hello, если не знаешь ответ, пиши 'хз'")
+print("Анаграмма: ", jumble)
+count = 0
+stat = 10
+print("Первая попытка без штрафа")
+guess = input("Пробуем: ")
+while guess !=correct and guess != "":
+    print("loh")
+    guess = input("now again")
+    if count == len(correct):
+        print("Ну ты и лох")
+        break
+    if guess == "хз":
+        print(count+1, "буква", correct[count])
+        stat -= 1
+        count += 1
+
+print("Твои баллы:", stat)
