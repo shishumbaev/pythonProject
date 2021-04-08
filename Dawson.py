dd4c7c7188a5e0932fb73b71b0f0912a09aa1bae
Shishumbaev <zhizhov1@gmail.com>
2021-04-08 12:53:19 +0300

    Start project day 2 rerun day 3 vse, krome my_var выбор результата Полностью переделал с 1 функцией гет working without global finish Начинаем работать по Доусону Программа отгадай число, отгадывает компьютер Счет интервал

--- Dawson.py
+++ Dawson.py
@@ -1,23 +1,4 @@
-number = int(input("Введите число от 1 до 100\n"))
-print(number)
-guess = 50
-max = 100
-min = 50
-count = 1
-print(guess)
-while guess != number:
-    if guess > number:
-        print("less then", guess)
-        max = guess
-        guess = min//2
-        print("new guess = ", guess)
-    elif guess < number:
-        print("more then", guess)
-
-        guess = (max-guess)//2+guess
-        min = guess
-        print("new guess = ", guess)
-
-    count += 1
-print("you're right!, the number is", guess)
-print("grats, you have", count, "tries")
+start, finish, interval = input("Введите начальное и конечное значения, а так же интервал:\n").split()
+print("Начало счета:", start, "Конец счета:", finish, "Интервал:", interval)
+for i in range(int(start),int(finish),int(interval)):
+    print(i)
