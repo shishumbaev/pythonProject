08e6885d6d77d49ccf1c883b3e343ce6f29c06b3
Shishumbaev <zhizhov1@gmail.com>
2021-04-09 08:16:08 +0300

    Start project day 2 rerun day 3 vse, krome my_var выбор результата Полностью переделал с 1 функцией гет working without global finish Начинаем работать по Доусону Программа отгадай число, отгадывает компьютер Счет интервал Обратная последовательность строки улучшенная анаграмма угадай слово

--- Dawson.py
+++ Dawson.py
@@ -1,29 +1,20 @@
-#АНАГРАММА
+#угадай слово
 import random
 WORDS = ("питон", "анаграмма", "простая", "сложная", "ответ", "подстаканник")
 word = random.choice(WORDS)
 correct = word
-jumble = ""
-while word:
-    position = random.randrange(len(word))
-    jumble += word[position]
-    word = word[:position] + word[(position+1):]
-
-print("Hello, если не знаешь ответ, пиши 'хз'")
-print("Анаграмма: ", jumble)
-count = 0
-stat = 10
-print("Первая попытка без штрафа")
-guess = input("Пробуем: ")
-while guess !=correct and guess != "":
+print("В слове", len(correct), "букв")
+guess = ""
+for i in range(5):
+    print("Введите букву: ")
+    guess = input()
+    if guess in word:
+        print("да")
+    else:
+        print("нет")
+print("Напишите слово целиком: ")
+answer = input()
+if answer == word:
+    print("YES")
+else:
     print("loh")
-    guess = input("now again")
-    if count == len(correct):
-        print("Ну ты и лох")
-        break
-    if guess == "хз":
-        print(count+1, "буква", correct[count])
-        stat -= 1
-        count += 1
-
-print("Твои баллы:", stat)
--- Test.py
+++ Test.py
@@ -1,50 +1,20 @@
-n = int(input())
-
-dict1 = {'None':['global']}
-dict2 = {'global': []}
-def add(namesp, arg):
-    if len(arg) <= 10:
-        if (str(namesp) in dict2.keys()):
-            dict2[namesp].append(str(arg))
-        else:
-            dict2[str(namesp)] = str(arg)
-
-def create(parent, namesp):
-    if len(namesp) and len(parent) <= 10:
-        if (str(namesp)) in dict1.keys():
-            dict1[namesp].append(str(parent))
-            dict2[parent] = []
-        else:
-            dict1[namesp] = [str(parent)]
-            dict2[parent] = []
-
-
-def get(namesp, arg):
-    parent = 0
-    for i in dict2.keys():
-        if i == namesp:
-            parent = namesp
-            for j in dict2[i]:
-                if arg == j:
-                    return parent
-
-            for i in dict1.keys():
-                for j in dict1[i]:
-                    if j == parent:
-                        parent = i
-                        return get(parent, arg)
-
-
-
-if 1<= n <= 100:
-    for i in range(n):
-        cmd, namesp, arg = input().split()
-        if cmd == 'add':
-            add(namesp, arg)
-        elif cmd == 'create':
-            create(namesp, arg)
-        elif cmd == 'get':
-            print(get(namesp, arg))
-
-print(dict1)
-print(dict2)
+#угадай слово
+import random
+WORDS = ("питон", "анаграмма", "простая", "сложная", "ответ", "подстаканник")
+word = random.choice(WORDS)
+correct = word
+print("В слове", len(correct), "букв")
+guess = ""
+print(word)
+for i in range(5):
+    guess = input()
+    if guess in word:
+        print("да")
+    else:
+        print("нет")
+print("Напишите слово целиком: ")
+answer = input()
+if answer == word:
+    print("YES")
+else:
+    print("loh")
